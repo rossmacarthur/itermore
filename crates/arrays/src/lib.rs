61deaa716e997c0a762d5e390d34dc3b2490de3b
Ross MacArthur <ross@macarthur.io>
2022-11-28 16:30:36 +0200

    crates: Add `arrays`

---
+++ crates/arrays/src/lib.rs
@@ -0,0 +1,100 @@
+//! Collect an iterator into an array
+//!
+//! # Getting started
+//!
+//! Add the `arrays` crate to your Cargo manifest.
+//!
+//! ```sh
+//! cargo add arrays
+//! ```
+//!
+//! Now collect any iterator into an array.
+//!
+//! ```
+//! # let iter = 1..5;
+//! let arr: [_; 3] = arrays::collect(iter).unwrap();
+//! ```
+
+#![no_std]
+#![warn(unsafe_op_in_unsafe_fn)]
+
+use core::mem;
+use core::mem::MaybeUninit;
+use core::ptr;
+
+/// Consumes `N` elements from the iterator and returns them as an array. If the
+/// iterator yields fewer than `N` items, `None` is returned and all already
+/// yielded items are dropped.
+///
+// Based on the array collect implementation in the Rust standard library.
+// https://github.com/rust-lang/rust/blob/master/library/core/src/array/mod.rs#L476-L531
+#[inline]
+pub fn collect<I, T, const N: usize>(mut iter: I) -> Option<[T; N]>
+where
+    I: Iterator<Item = T>,
+{
+    struct Guard<'a, T, const N: usize> {
+        array: &'a mut [MaybeUninit<T>; N],
+        init: usize,
+    }
+
+    impl<T, const N: usize> Drop for Guard<'_, T, N> {
+        fn drop(&mut self) {
+            for elem in &mut self.array.as_mut_slice()[..self.init] {
+                // SAFETY: this raw slice up to `self.len` will only contain
+                // the initialized objects.
+                unsafe { ptr::drop_in_place(elem.as_mut_ptr()) };
+            }
+        }
+    }
+
+    // SAFETY: The `assume_init` is safe because the type we are claiming to
+    // have initialized here is a bunch of `MaybeUninit`s, which do not
+    // require initialization.
+    //
+    // This is not the most ideal way of doing this. In the future when Rust
+    // allows inline const expressions we might be able to use the following.
+    //
+    //      let mut array = [const { MaybeUninit::<T>::uninit() }; N];
+    //
+    // See https://doc.rust-lang.org/std/mem/union.MaybeUninit.html#initializing-an-array-element-by-element
+    let mut array: [MaybeUninit<T>; N] = unsafe { MaybeUninit::uninit().assume_init() };
+
+    let mut guard = Guard {
+        array: &mut array,
+        init: 0,
+    };
+
+    for _ in 0..N {
+        match iter.next() {
+            Some(item) => {
+                // SAFETY: `guard.init` starts at zero, is increased by 1 each
+                // iteration of the loop, and the loop is aborted once M * N
+                // is reached, which is the length of the array.
+                unsafe { guard.array.get_unchecked_mut(guard.init).write(item) };
+                guard.init += 1;
+            }
+            None => {
+                return None;
+                // <-- guard is dropped here with already initialized elements
+            }
+        }
+    }
+
+    mem::forget(guard);
+    // SAFETY: the loop above loops exactly N times which is the size of the
+    // array, so all elements in the array are initialized.
+    Some(unsafe { transmute_unchecked(array) })
+}
+
+/// Size-heterogeneous transmutation.
+///
+/// This is required because the compiler doesn't yet know how to deal with the
+/// size of const arrays. We should be able to use [`mem::transmute()`] but it
+/// doesn't work yet :(.
+#[inline]
+unsafe fn transmute_unchecked<A, B>(a: A) -> B {
+    let b = unsafe { ptr::read(&a as *const A as *const B) };
+    mem::forget(a);
+    b
+}
--- crates/chunks/src/lib.rs
+++ crates/chunks/src/lib.rs
@@ -35,10 +35,6 @@
 #![no_std]
 #![warn(unsafe_op_in_unsafe_fn)]
 
-use core::mem;
-use core::mem::MaybeUninit;
-use core::ptr;
-
 /// An extension trait that provides the [`chunks`][IterChunks::chunks]
 /// method for iterators.
 pub trait IterChunks: Iterator {
@@ -78,7 +74,7 @@ pub trait IterChunks: Iterator {
     where
         Self: Sized,
     {
-        collect(self.by_ref())
+        arrays::collect(self.by_ref())
     }
 
     /// Returns an iterator over `N` elements of the iterator at a time.
@@ -151,7 +147,7 @@ where
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         let Self { iter } = self;
-        collect(iter.by_ref())
+        arrays::collect(iter.by_ref())
     }
 
     #[inline]
@@ -178,85 +174,4 @@ where
         chunk.reverse();
         Some(chunk)
     }
-}
-
-/// Consumes `N` elements from the iterator and returns them as an array. If the
-/// iterator yields fewer than `N` items, `None` is returned and all already
-/// yielded items are dropped.
-///
-/// Since the iterator is passed as a mutable reference and this function calls
-/// `next` at most `N` times, the iterator can still be used afterwards to
-/// retrieve the remaining items.
-///
-// Based on the array collect implementation in the Rust standard library.
-// https://github.com/rust-lang/rust/blob/master/library/core/src/array/mod.rs#L476-L531
-#[inline]
-fn collect<I, T, const N: usize>(mut iter: I) -> Option<[T; N]>
-where
-    I: Iterator<Item = T>,
-{
-    struct Guard<'a, T, const N: usize> {
-        array: &'a mut [MaybeUninit<T>; N],
-        init: usize,
-    }
-
-    impl<T, const N: usize> Drop for Guard<'_, T, N> {
-        fn drop(&mut self) {
-            for elem in &mut self.array.as_mut_slice()[..self.init] {
-                // SAFETY: this raw slice up to `self.len` will only contain
-                // the initialized objects.
-                unsafe { ptr::drop_in_place(elem.as_mut_ptr()) };
-            }
-        }
-    }
-
-    // SAFETY: The `assume_init` is safe because the type we are claiming to
-    // have initialized here is a bunch of `MaybeUninit`s, which do not
-    // require initialization.
-    //
-    // This is not the most ideal way of doing this. In the future when Rust
-    // allows inline const expressions we might be able to use the following.
-    //
-    //      let mut array = [const { MaybeUninit::<T>::uninit() }; N];
-    //
-    // See https://doc.rust-lang.org/std/mem/union.MaybeUninit.html#initializing-an-array-element-by-element
-    let mut array: [MaybeUninit<T>; N] = unsafe { MaybeUninit::uninit().assume_init() };
-
-    let mut guard = Guard {
-        array: &mut array,
-        init: 0,
-    };
-
-    for _ in 0..N {
-        match iter.next() {
-            Some(item) => {
-                // SAFETY: `guard.init` starts at zero, is increased by 1 each
-                // iteration of the loop, and the loop is aborted once M * N
-                // is reached, which is the length of the array.
-                unsafe { guard.array.get_unchecked_mut(guard.init).write(item) };
-                guard.init += 1;
-            }
-            None => {
-                return None;
-                // <-- guard is dropped here with already initialized elements
-            }
-        }
-    }
-
-    mem::forget(guard);
-    // SAFETY: the loop above loops exactly N times which is the size of the
-    // array, so all elements in the array are initialized.
-    Some(unsafe { transmute_unchecked(array) })
-}
-
-/// Size-heterogeneous transmutation.
-///
-/// This is required because the compiler doesn't yet know how to deal with the
-/// size of const arrays. We should be able to use [`mem::transmute()`] but it
-/// doesn't work yet :(.
-#[inline]
-unsafe fn transmute_unchecked<A, B>(a: A) -> B {
-    let b = unsafe { ptr::read(&a as *const A as *const B) };
-    mem::forget(a);
-    b
 }
