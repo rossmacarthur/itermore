d15606d9067e32f938b10ebb2862fa92075f05c8
Ross MacArthur <ross@macarthur.io>
2025-03-14 14:25:58 +0200

    Use non-colliding name for array chunks in tests

--- tests/array_chunks.rs
+++ tests/array_chunks.rs
@@ -7,13 +7,13 @@ use itermore::prelude::*;
 
 #[test]
 fn array_chunks_debug() {
-    let iter = (0..6).array_chunks::<2>();
+    let iter = (0..6).arrays::<2>();
     let _ = format!("{:?}", iter);
 }
 
 #[test]
 fn array_chunks_clone() {
-    let mut iter = (0..6).array_chunks::<2>();
+    let mut iter = (0..6).arrays::<2>();
     let mut iter2 = iter.clone();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter2.next(), Some([0, 1]));
@@ -22,78 +22,78 @@ fn array_chunks_clone() {
 #[test]
 fn array_chunks_infer() {
     let s = [1, 1, 2, -2, 6, 0, 3, 1];
-    for [a, b, c] in s.iter().copied().array_chunks() {
+    for [a, b, c] in s.iter().copied().arrays() {
         assert_eq!(a + b + c, 4);
     }
 }
 
 #[test]
 fn array_chunks_size_hint() {
-    let iter = (0..6).array_chunks::<1>();
+    let iter = (0..6).arrays::<1>();
     assert_eq!(iter.size_hint(), (6, Some(6)));
 
-    let iter = (0..6).array_chunks::<3>();
+    let iter = (0..6).arrays::<3>();
     assert_eq!(iter.size_hint(), (2, Some(2)));
 
-    let iter = (0..6).array_chunks::<5>();
+    let iter = (0..6).arrays::<5>();
     assert_eq!(iter.size_hint(), (1, Some(1)));
 
-    let iter = (0..6).array_chunks::<7>();
+    let iter = (0..6).arrays::<7>();
     assert_eq!(iter.size_hint(), (0, Some(0)));
 
-    let iter = (1..).array_chunks::<2>();
+    let iter = (1..).arrays::<2>();
     assert_eq!(iter.size_hint(), (usize::MAX / 2, None));
 
-    let iter = (1..).filter(|x| x % 2 != 0).array_chunks::<2>();
+    let iter = (1..).filter(|x| x % 2 != 0).arrays::<2>();
     assert_eq!(iter.size_hint(), (0, None));
 }
 
 #[test]
 fn array_chunks_count() {
-    let iter = (0..6).array_chunks::<1>();
+    let iter = (0..6).arrays::<1>();
     assert_eq!(iter.count(), 6);
 
-    let iter = (0..6).array_chunks::<3>();
+    let iter = (0..6).arrays::<3>();
     assert_eq!(iter.count(), 2);
 
-    let iter = (0..6).array_chunks::<5>();
+    let iter = (0..6).arrays::<5>();
     assert_eq!(iter.count(), 1);
 
-    let iter = (0..6).array_chunks::<7>();
+    let iter = (0..6).arrays::<7>();
     assert_eq!(iter.count(), 0);
 
-    let iter = (0..6).filter(|x| x % 2 == 0).array_chunks::<2>();
+    let iter = (0..6).filter(|x| x % 2 == 0).arrays::<2>();
     assert_eq!(iter.count(), 1);
 
-    let iter = iter::empty::<i32>().array_chunks::<2>();
+    let iter = iter::empty::<i32>().arrays::<2>();
     assert_eq!(iter.count(), 0);
 
-    let iter = [(); usize::MAX].iter().array_chunks::<2>();
+    let iter = [(); usize::MAX].iter().arrays::<2>();
     assert_eq!(iter.count(), usize::MAX / 2);
 }
 
 #[test]
 fn array_chunks_remainder() {
-    let mut iter = (0..4).array_chunks::<2>();
+    let mut iter = (0..4).arrays::<2>();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter.next(), Some([2, 3]));
     assert_eq!(iter.next(), None);
     assert_eq!(iter.into_remainder().collect::<Vec<_>>(), []);
 
-    let mut iter = (0..5).array_chunks::<2>();
+    let mut iter = (0..5).arrays::<2>();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter.next(), Some([2, 3]));
     assert_eq!(iter.next(), None);
     assert_eq!(iter.into_remainder().collect::<Vec<_>>(), [4]);
 
-    let mut iter = (0..5).array_chunks::<2>();
+    let mut iter = (0..5).arrays::<2>();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter.into_remainder().collect::<Vec<_>>(), [4]);
 }
 
 #[test]
 fn array_chunks_next_back() {
-    let mut iter = (0..7).array_chunks::<2>();
+    let mut iter = (0..7).arrays::<2>();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter.next_back(), Some([4, 5]));
     assert_eq!(iter.next(), Some([2, 3]));
@@ -103,7 +103,7 @@ fn array_chunks_next_back() {
 
 #[test]
 fn array_chunks_next_back_remainder() {
-    let mut iter = (0..7).array_chunks::<2>();
+    let mut iter = (0..7).arrays::<2>();
     assert_eq!(iter.next(), Some([0, 1]));
     assert_eq!(iter.next_back(), Some([4, 5]));
     assert_eq!(iter.next(), Some([2, 3]));
@@ -113,7 +113,7 @@ fn array_chunks_next_back_remainder() {
 #[allow(clippy::iter_nth_zero)]
 #[test]
 fn array_chunks_nth() {
-    let mut iter = (0..6).array_chunks::<2>();
+    let mut iter = (0..6).arrays::<2>();
     assert_eq!(iter.nth(1), Some([2, 3]));
     assert_eq!(iter.nth(0), Some([4, 5]));
     assert_eq!(iter.nth(1), None);
@@ -121,30 +121,30 @@ fn array_chunks_nth() {
 
 #[test]
 fn array_chunks_len() {
-    let iter = (0..6).array_chunks::<1>();
+    let iter = (0..6).arrays::<1>();
     assert_eq!(iter.len(), 6);
 
-    let iter = (0..6).array_chunks::<2>();
+    let iter = (0..6).arrays::<2>();
     assert_eq!(iter.len(), 3);
 
-    let iter = (0..6).array_chunks::<3>();
+    let iter = (0..6).arrays::<3>();
     assert_eq!(iter.len(), 2);
 
-    let iter = (0..6).array_chunks::<4>();
+    let iter = (0..6).arrays::<4>();
     assert_eq!(iter.len(), 1);
 
-    let iter = (0..6).array_chunks::<5>();
+    let iter = (0..6).arrays::<5>();
     assert_eq!(iter.len(), 1);
 
-    let iter = (0..6).array_chunks::<6>();
+    let iter = (0..6).arrays::<6>();
     assert_eq!(iter.len(), 1);
 
-    let iter = (0..6).array_chunks::<7>();
+    let iter = (0..6).arrays::<7>();
     assert_eq!(iter.len(), 0);
 
-    let iter = iter::empty::<i32>().array_chunks::<2>();
+    let iter = iter::empty::<i32>().arrays::<2>();
     assert_eq!(iter.len(), 0);
 
-    let iter = [(); usize::MAX].iter().array_chunks::<2>();
+    let iter = [(); usize::MAX].iter().arrays::<2>();
     assert_eq!(iter.len(), usize::MAX / 2);
 }
